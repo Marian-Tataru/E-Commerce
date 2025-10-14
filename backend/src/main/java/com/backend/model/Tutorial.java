e3cfba1b5bd7ce9adc181bbc3ee4a11e7eb5a0a1
marian <tatarumarian2001@gmail.com>
2025-10-14 21:22:39 +0200

    test docker compose functionalities

--- backend/src/main/java/com/backend/model/Tutorial.java
+++ backend/src/main/java/com/backend/model/Tutorial.java
@@ -1,6 +1,11 @@
-package com.bezkoder.spring.jpa.postgresql.model;
+package com.backend.model;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "tutorials")
